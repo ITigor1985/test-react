6b5306644a48511f71bde9b1bc65ae3661d41abe
ITigor1985 <xromer85@gmail.com>
2022-07-14 13:18:56 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
+import "./App.css";
+//import Form from "./components/Form";
+import { useTranslation, Trans } from "react-i18next";
 
-import './App.css';
-import Form from './components/Form';
+const lngs = {
+  en: { nativeName: "English" },
+  de: { nativeName: "Deutsch" },
+};
 
 function App() {
+  // return <div className="App">{/* <Form/> */}</div>;
+  const { t, i18n } = useTranslation();
+
   return (
     <div className="App">
-      <Form/>
+      <header className="App-header">
+        <div>
+          {Object.keys(lngs).map((lng) => (
+            <button
+              key={lng}
+              style={{
+                fontWeight: i18n.resolvedLanguage === lng ? "bold" : "normal",
+              }}
+              type="submit"
+              onClick={() => i18n.changeLanguage(lng)}
+            >
+              {lngs[lng].nativeName}
+            </button>
+          ))}
+        </div>
+        <p>
+          <Trans i18nKey="description.part1">
+            Edit <code>src/App.js</code> and save to reload.
+          </Trans>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {t("description.part2")}
+        </a>
+      </header>
     </div>
   );
 }
